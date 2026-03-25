01edd86ef1fabeef071cddb8d7d9c1ea33fd895f
Dand <artem2skazu@gmail.com>
2026-03-25 20:40:51 +0200

    Update Node.cs

--- StudyHub/Models/Node.cs
+++ StudyHub/Models/Node.cs
@@ -1,6 +1,42 @@
-﻿namespace StudyHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyHub.Models
 {
-    public class Node
+    public enum NoteStatus
     {
+        Pending,   
+        Approved,  
+        Rejected   
+    }
+
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введіть назву конспекту")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Текст конспекту не може бути порожнім")]
+        public string Content { get; set; }
+
+        [Required]
+        [Range(1, 4, ErrorMessage = "Курс має бути від 1 до 4")]
+        public int Course { get; set; }
+
+        [Required]
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+        [Required]
+        public string AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public NoteStatus Status { get; set; } = NoteStatus.Pending;
+
+        public int LikesCount { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;      
     }
 }
